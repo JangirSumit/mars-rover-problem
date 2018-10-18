17f1032bf1817ab289c84d555669fd65120007a4
Sumit Jangir (XSUMJ) <XSUMJ@saxobank.com>
2018-10-18 10:19:12 +0530

    code seperation

---
+++ MarsRoverProblem/Position.cs
@@ -0,0 +1,86 @@
+﻿namespace MarsRoverProblem
+{
+    public enum Directions
+    {
+        N = 1,//North
+        S = 2,//South
+        E = 3,//East
+        W = 4//West
+    }
+
+    public class Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Directions Direction { get; set; }
+
+        public Position()
+        {
+            X = Y = 0;
+            Direction = Directions.N;
+        }
+
+        public void Rotate90Left()
+        {
+            switch (this.Direction)
+            {
+                case Directions.N:
+                    this.Direction = Directions.W;
+                    break;
+                case Directions.S:
+                    this.Direction = Directions.E;
+                    break;
+                case Directions.E:
+                    this.Direction = Directions.N;
+                    break;
+                case Directions.W:
+                    this.Direction = Directions.S;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void Rotate90Right()
+        {
+            switch (this.Direction)
+            {
+                case Directions.N:
+                    this.Direction = Directions.E;
+                    break;
+                case Directions.S:
+                    this.Direction = Directions.W;
+                    break;
+                case Directions.E:
+                    this.Direction = Directions.S;
+                    break;
+                case Directions.W:
+                    this.Direction = Directions.N;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void MoveInSameDirection()
+        {
+            switch (this.Direction)
+            {
+                case Directions.N:
+                    this.Y += 1;
+                    break;
+                case Directions.S:
+                    this.Y -= 1;
+                    break;
+                case Directions.E:
+                    this.X += 1;
+                    break;
+                case Directions.W:
+                    this.X -= 1;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
